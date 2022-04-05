6a48adb96f2eafc2150b5f969f3d8cd37b8c1ac8
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2022-04-06 00:30:12 +0530

    Create User.java

---
+++ AuthService/src/main/java/com/electrogrid/entity/User.java
@@ -0,0 +1,55 @@
+package com.electrogrid.entity;
+
+/**
+ * Created By lonewol7f
+ * Date: 4/5/2022
+ */
+public class User {
+
+    private int id;
+    private String role;
+    private String username;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String role, String username, String password) {
+        this.id = id;
+        this.role = role;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
