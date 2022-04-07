7567f5fbd54fb04565358abb454f5eb067cdeb99
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2022-04-07 13:26:08 +0530

    Update AuthServiceI.java

--- AuthService/src/main/java/com/electrogrid/service/AuthServiceI.java
+++ AuthService/src/main/java/com/electrogrid/service/AuthServiceI.java
@@ -1,5 +1,6 @@
 package com.electrogrid.service;
 
+import com.electrogrid.entity.Credentials;
 import com.electrogrid.entity.User;
 
 import javax.ws.rs.core.Response;
@@ -10,5 +11,6 @@ import javax.ws.rs.core.Response;
  */
 public interface AuthServiceI {
     Response insertUser(User user);
+    Response login(Credentials credentials);
     boolean isUserExist(String email);
 }
