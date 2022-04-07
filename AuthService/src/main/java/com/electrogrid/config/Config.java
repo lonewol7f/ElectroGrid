3e3bfefd9784f7a2ee3abac0f37535e85f63ce2b
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2022-04-07 06:58:14 +0530

    Refactor config.java

--- AuthService/src/main/java/com/electrogrid/config/Config.java
+++ AuthService/src/main/java/com/electrogrid/config/Config.java
@@ -1,6 +1,7 @@
 package com.electrogrid.config;
 
 import com.electrogrid.controller.HomeController;
+import com.electrogrid.controller.UserController;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -21,11 +22,29 @@ public class Config extends Application {
     public static final String DB_PROPERTIES_FILE = "db.properties";
     public static Properties properties = new Properties();
 
+    private final Set<Class<?>> classes;
+    private final Set<Object> singletons;
+
+
+    public Config() {
+        readProperties();
+
+        this.classes = new HashSet<>();
+        this.classes.add(HomeController.class);
+        this.classes.add(UserController.class);
+
+        this.singletons = new HashSet<>();
+        this.singletons.add(new SecurityFilter());
+    }
+
     @Override
     public Set<Class<?>> getClasses() {
-        final Set<Class<?>> classes = new HashSet<>();
-        classes.add(HomeController.class);
-        return classes;
+        return this.classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return this.singletons;
     }
 
     public void readProperties() {
