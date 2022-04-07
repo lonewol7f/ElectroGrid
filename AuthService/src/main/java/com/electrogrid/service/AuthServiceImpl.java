b14ada9f83e0847600da066b4f536bd27716f64e
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2022-04-07 13:26:13 +0530

    Update AuthServiceImpl.java

--- AuthService/src/main/java/com/electrogrid/service/AuthServiceImpl.java
+++ AuthService/src/main/java/com/electrogrid/service/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.electrogrid.service;
 
+import com.electrogrid.entity.Credentials;
 import com.electrogrid.entity.User;
 import com.electrogrid.util.DBUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,10 +8,12 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.mindrot.jbcrypt.BCrypt;
 
 import javax.ws.rs.core.Response;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Base64;
 
 /**
  * Created By lonewol7f
@@ -61,6 +64,67 @@ public class AuthServiceImpl implements AuthServiceI {
                         .entity(json).build();
 
                 conn.close();
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            ObjectNode json = mapper.createObjectNode();
+            json.put("error", e.getMessage());
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(json).build();
+        }
+
+        return response;
+    }
+
+    @Override
+    public Response login(Credentials credentials) {
+
+        Response response = null;
+
+        if (!isUserExist(credentials.getUsername())) {
+            ObjectNode json = mapper.createObjectNode();
+            json.put("error", "User not found !!!");
+
+            return Response.status(Response.Status.CREATED)
+                    .entity(json).build();
+        }
+
+        try {
+            Connection conn = DBUtil.connect();
+
+            if (conn != null) {
+                String query = "SELECT * FROM users WHERE email= ?";
+
+                PreparedStatement stmt = conn.prepareStatement(query);
+                stmt.setString(1, credentials.getUsername());
+
+                ResultSet rs = stmt.executeQuery();
+
+                String password = "";
+                if (rs.next()) {
+                    password = rs.getString("password");
+                }
+
+                if (!BCrypt.checkpw(credentials.getPassword(), password)) {
+                    ObjectNode json = mapper.createObjectNode();
+                    json.put("error", "Credentials does not match");
+
+                    return Response.status(Response.Status.CREATED)
+                            .entity(json).build();
+                }
+
+                // TODO: issue token
+                String token = credentials.getUsername() + ":" + credentials.getPassword();
+                String encodedToken = "Basic " + Base64.getEncoder().encodeToString(token.getBytes(StandardCharsets.UTF_8));
+
+                System.out.println(encodedToken);
+
+                ObjectNode json = mapper.createObjectNode();
+                json.put("success", "User logged in");
+
+                return Response.status(200)
+                        .entity(json)
+                        .header("Authorization", encodedToken).build();
             }
 
         } catch (SQLException | ClassNotFoundException e) {
