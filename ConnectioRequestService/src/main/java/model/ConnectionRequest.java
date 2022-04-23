e29417dc65aaa46afb56ce66e5e0f868baa6c7df
deadp00l97 <hasinthadhanushka@gmail.com>
2022-04-23 22:39:19 +0530

    Create insert connection request

--- ConnectioRequestService/src/main/java/model/ConnectionRequest.java
+++ ConnectioRequestService/src/main/java/model/ConnectionRequest.java
@@ -21,5 +21,45 @@ public class ConnectionRequest {
         return con;
     }
 
+    //Insert connection request
+    public String insertConnRequest(String nicNo , String firstName , String lastName , String address , String tpNo) {
+
+        String output = "";
+
+        try {
+
+            Connection con = connect();
+
+            if(con == null) {
+                return "error while connecting database"; }
+
+
+            // create a prepared statement
+            String sql = "insert into connection_req(`connReqId`, `nicNo`, `firstName`, `lastName`, `address`, `tpNo`)"
+                    + "values (?,?,?,?,?,?)";
+
+            PreparedStatement state = con.prepareStatement(sql);
+
+            // binding values
+            state.setInt(1, 0);
+            state.setString(2, nicNo);
+            state.setString(3, firstName);
+            state.setString(4, lastName);
+            state.setString(5, address);
+            state.setString(6, tpNo);
+
+            //execute the statement
+            state.execute();
+            con.close();
+
+            output = "Inserted successfully";
+
+        }catch(Exception e) {
+            output = "Error while inserting the connection request.";
+            System.err.println(e.getMessage()); }
+        return output;
+    }
+
+
 
 }
