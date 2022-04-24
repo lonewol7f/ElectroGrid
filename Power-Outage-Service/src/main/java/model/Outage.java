d09408469a8d80da7422b40a9bceda920c269fa5
Sachintha Nirmal <sachintha919@gmail.com>
2022-04-24 17:49:16 +0530

    create READ operation

--- Power-Outage-Service/src/main/java/model/Outage.java
+++ Power-Outage-Service/src/main/java/model/Outage.java
@@ -49,6 +49,70 @@ public class Outage {
 		return output;
 	}
 	
-	
+	//read Data
+		public String readOutages() {
+			String output  = "";
+			
+			try {
+				Connection con = DBConnect.connect();
+				
+				if (con == null) {
+					return "error while connecting database for reading";
+				}
+				
+				// Prepare the html table to be displayed
+				 output = "<table border='1'><tr><th>Customer ID</th>" 
+				 +"<th>Customer Name</th><th>Outage Area</th>"
+				 + "<th>Outage Date</th>"  + "<th>Outage Time</th>" + "<th>Outage Description</th>"
+				 + "<th>Update</th><th>Remove</th></tr>";
+				 
+				 String query = "select * from outage"; 
+				 Statement stmt = con.createStatement(); 
+				 ResultSet rs = stmt.executeQuery(query);
+				 
+				 while (rs.next()) 
+				 { 
+					 String outageID = Integer.toString(rs.getInt("outageID")); 
+					 String cusID = Integer.toString(rs.getInt("cusID")); 
+					 String cusName = rs.getString("cusName"); 
+					 String outArea =  rs.getString("outArea"); 
+					 String outDate = rs.getString("outDate"); 
+	                 String outTime = rs.getString("outTime");
+	                 String outDesc = rs.getString("outDesc");
+					 // Add a row into the html table
+					 
+					 output += "<tr><td>" + cusID + "</td>"; 
+					 output += "<td>" + cusName + "</td>"; 
+					 output += "<td>" + outArea + "</td>";
+					 output += "<td>" + outDate + "</td>"; 
+	                 output += "<td>" + outTime + "</td>";
+	                 output += "<td>" + outDesc + "</td>";
+					 
+					 // buttons
+					 output += "<td><input name='btnUpdate' " 
+							 + " type='button' value='Update'></td>"
+							 + "<td><form method='post' action='outages.jsp'>"
+							 + "<input name='btnRemove' " 
+							 + " type='submit' value='Remove'>"
+							 + "<input name='outageID' type='hidden' " 
+							 + " value='" + outageID + "'>" + "</form></td></tr>"; 
+				 } 
+				 
+				 con.close(); 
+				 // Complete the html table
+				 output += "</table>"; 
+				 
+				 
+			}catch(Exception e) {
+				
+				output = "error while reading outages";
+				System.err.println(e.getMessage());
+				
+			}
+			
+			return output;
+		}
+		
+		
 	
 }
